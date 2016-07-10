ee2852264bfff3b9d6c517d337fc1b01eca00800
JuliaBond <developer.julia.bondarenko@gmail.com>
2016-07-11 00:50:34 +1200

    module5-solution

---
+++ module5-practice/Lecture58/js/script.js
@@ -0,0 +1,55 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+  /*      $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesChineseFood) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+*/
+        $ajaxUtils
+          .sendGetRequest("https://github.com/emarref/bnz-netguard-card/blob/master/src/manifest.json", 
+            function (res) {
+              var message = 
+                res.content_scripts; /*+ " " + res.lastName
+              if (res.likesChineseFood) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";*/
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + message + "</h2>";
+            });
+
+      });
+  }
+);
+
+
+
+
+
